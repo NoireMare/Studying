642b79cb424a3f4437ac588016e48b063c20cde8
NoireMare <109278616+NoireMare@users.noreply.github.com>
2022-12-24 16:50:07 +0200

    Home work D16.4

---
+++ newsPortal D16.4/news/forms.py
@@ -0,0 +1,16 @@
+from django import forms
+from .models import Post
+
+
+
+class PostForm(forms.ModelForm):
+    rating = forms.FloatField(min_value=0, max_value=5, label="Рейтинг")
+    class Meta:
+        model = Post
+        fields = ['title', 'text', 'rating', 'post_author', 'category_post']
+        labels = {'title': 'Название', 'text': "Текст", 'post_author': "Автор текста", 'category_post': 'Категория'}
+
+
+
+
+
